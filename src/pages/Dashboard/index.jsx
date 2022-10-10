7bd4fa99306718c19ea500d923234927618903d9
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2022-10-10 09:11:07 -0300

    Add files via upload

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import { useLocalStorage, useAsyncFn } from 'react-use'
+import { format, formatISO } from 'date-fns'
+import axios from 'axios'
+
+import { Icon, Card, DateSelect } from "~/components"
+
+export const Dashboard = () => {
+  const [currentDate, setDate] = useState(formatISO(new Date(2022, 10, 20)))
+  const [auth] = useLocalStorage('auth', {})
+
+  const [{ value: user, loading, error }, fetchHunches] = useAsyncFn(async () => {
+    const res = await axios({
+      method: 'get',
+      baseURL: import.meta.env.VITE_API_URL,
+      url: `/${auth.user.username}`
+    })
+
+    const hunches = res.data.hunches.reduce((acc, hunch) => {
+      acc[hunch.gameId] = hunch
+      return acc
+    }, {})
+
+    return {
+      ...res.data,
+      hunches
+    }
+  })
+
+  const [games, fetchGames] = useAsyncFn(async (params) => {
+    const res = await axios({
+      method: 'get',
+      baseURL: import.meta.env.VITE_API_URL,
+      url: '/games',
+      params
+    })
+
+    return res.data
+  })
+
+  const isLoading = games.loading || loading
+  const hasError = games.error || error
+  const isDone = !isLoading && !hasError
+
+
+  useEffect(() => {
+    fetchHunches()
+  }, [])
+
+
+  useEffect(() => {
+    fetchGames({ gameTime: currentDate })
+  }, [currentDate])
+
+  if (!auth?.user?.id) {
+    return <Navigate to="/" replace={true} />
+  }
+
+  return (
+    // O Fragment para quando temos vários elementos ele engloba tudo, vem do próprio React com um import { Fragment } from 'react', mas ele também entende apenas colocando <> e fechando no final com </>
+    <>
+      <header className="bg-red-500 text-white">
+        <div className="container max-w-3xl flex justify-between p-4">
+          <img src="./imgs/logo-black.svg" className="w-2832 md:w-40" />
+          <a href={`/${auth?.user?.username}`}>
+            <Icon name="profile" className="w-10" />
+          </a>
+        </div>
+      </header>
+
+      <main className="space-y-6">
+        <section id="header" className=" bg-red-500 text-white">
+          <div className="container max-w-3xl space-y-2 p-4">
+            <span>{auth.user.name}</span>
+            <h3 className="text-2xl font-bold">Qual é o seu palpite?</h3>
+          </div>
+        </section>
+
+        <section id="content" className="container max-w-3xl p-4 space-y-4">
+
+          <DateSelect currentDate={currentDate} onChange={setDate} />
+
+          <div className="space-y-4">
+            {isLoading && 'Carregando jogos...'}
+            {hasError && 'Ops! Algo deu errado.'}
+
+            {isDone && games.value?.map(game => (
+              <Card
+                key={game.id}
+                gameId={game.id}
+                homeTeam={game.homeTeam}
+                awayTeam={game.awayTeam}
+                gameTime={format(new Date(game.gameTime), 'H:mm')}
+                homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore || ''}
+                awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore || ''}
+
+              />
+            ))}
+
+
+          </div>
+
+        </section>
+      </main>
+
+    </>
+  )
+}
---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,111 @@
+import axios from 'axios'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { Navigate } from 'react-router-dom'
+import { useLocalStorage } from 'react-use'
+
+import { Icon, Input } from '~/components'
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required('Preencha seu nome'),
+  username: yup.string().required('Preencha seu nome de usuário'),
+  email: yup.string().email('Informe um email válido').required('Informe seu email'),
+  password: yup.string().required('Digite sua senha')
+});
+
+export const Signup = () => {
+
+  const [auth, setAuth] = useLocalStorage('auth', {})
+
+  const formik = useFormik({
+    onSubmit: async (values) => {
+
+    const res = await axios({
+        method: 'post',
+        baseURL: import.meta.env.VITE_API_URL,
+        url: '/users',
+        data: values
+      })
+
+      console.log(res.data)
+
+    },
+    initialValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: ''
+    },
+    validationSchema
+  })
+
+  if (auth?.user?.id) {
+    return <Navigate to="/dashboard" replace={true} />
+  }
+
+  return (
+    <div>
+      <header className="p-4 border-b border-red-300">
+        <div className="container max-w-xl flex justify-center">
+          <img src="./imgs/logo.svg" className="w-32 md:w-40" />
+        </div>
+      </header>
+      <main className="container max-w-xl p-4">
+        <div className="p-4 flex space-x-4 items-center">
+          <a href="/">
+            <Icon name="back" className="h-6" />
+          </a>
+          <h2 className="text-xl font-bold">Crie a sua conta</h2>
+        </div>
+
+        <form className="p-4 space-y-6" onSubmit={formik.handleSubmit}>
+          <Input
+            type="text"
+            name="name"
+            label="Seu nome"
+            placeholder="Digite seu nome"
+            error={formik.touched.name && formik.errors.name}
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+
+          <Input
+            type="text"
+            name="username"
+            label="Seu nome de usuário"
+            placeholder="Digite um nome de usuário"
+            error={formik.touched.username && formik.errors.username}
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+
+
+          <Input
+            type="text"
+            name="email"
+            label="Seu e-mail"
+            placeholder="Digite seu email"
+            error={formik.touched.email && formik.errors.email}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+
+          <Input
+            type="password"
+            name="password"
+            label="Sua senha"
+            placeholder="Digite sua senha"
+            error={formik.touched.password && formik.errors.password}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <button type='submit' className="w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl block disabled:opacity-50" disabled={!formik.isValid || formik.isSubmitting}>{formik.isSubmitting ? 'Criando...' : 'Criar minha conta'}</button>
+        </form>
+      </main>
+    </div>
+  )
+}
